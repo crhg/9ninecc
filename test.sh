a4ce28e04a1bb866babd26c280b8bacbd0ce343d
Manabu Matsui <manabu.matsui@gmail.com>
2019-05-29 22:17:17 +0900

    while文のコード生成のバグを再現するテスト

--- test.sh
+++ test.sh
@@ -53,6 +53,7 @@ try 3 "if(0)return 2; return 3;"
 try 2 "if(1)return 2;else return 3;return 4;"
 try 3 "if(0)return 2;else return 3;return 4;"
 try 5 "a=0;while(a != 5) a=a+1; return a;"
+#try 0 "a=0;while(a != 99999999) a=a+1; return 0;" # segvする。たぶんループ毎にスタックが伸びている
 
 echo OK
 
