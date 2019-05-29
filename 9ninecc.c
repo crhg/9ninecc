96076c6f288e8d5fb66a64704be4e351fb97b848
Manabu Matsui <manabu.matsui@gmail.com>
2019-05-29 11:48:51 +0900

    構文木を使ったコードジェネレータを導入(加減算のみ)

--- 9ninecc.c
+++ 9ninecc.c
@@ -17,12 +17,55 @@ typedef struct {
     char *input; //トークン文字列(エラーメッセージ用)
 } Token;
 
+// ノードの型を表す値
+enum {
+    ND_NUM = 256, // 整数のノードの型
+};
+
+// ノードの型
+typedef struct Node {
+    int ty;
+    struct Node *lhs;
+    struct Node *rhs;
+    int val; // tyがND_NUMの場合のみ使う
+} Node;
+
+// 2項演算子のノードを作る
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+    Node *node = malloc(sizeof(Node));
+    node->ty = ty;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+// 数値のノードを作る
+Node *new_node_num(int val) {
+    Node *node = malloc(sizeof(Node));
+    node->ty = ND_NUM;
+    node->val = val;
+    return node;
+}
+
 // 入力プログラム
 char *user_input;
 
-// トーク内寿司多結果のトークン列はこの配列に保存する
+// トーク結果のトークン列はこの配列に保存する
 // 100個以上のトークンは来ないものとする
 Token tokens[100];
+
+
+int pos = 0;
+
+// 次のトークンが期待した値かチェックして
+// 期待した値の場合だけ入力を1トークン読み進めてそのトークンを返す
+// それ以外はNULLを返す
+Token *consume(int ty) {
+    if (tokens[pos].ty != ty)
+        return NULL;
+
+    return &tokens[pos++];
+}
 
 // エラーを報告するための関数
 // printfと同じ引数を取る
@@ -79,6 +122,52 @@ void tokenize() {
     tokens[i].input = p;
 }
 
+// 式のパーサ
+Node *expr() {
+    Token *num;
+    if ((num = consume(TK_NUM)) == NULL)
+        error_at(tokens[pos].input, "数でありません");
+
+    Node *node = new_node_num(num->val);
+
+    for (;;) {
+        if (consume('+'))
+            node = new_node('+', node, expr());
+        else if (consume('-'))
+            node = new_node('-', node, expr());
+        else
+            return node;
+    }
+}
+
+// コード生成
+void gen(Node *node) {
+    if (node->ty == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->ty) {
+    case '+':
+        printf("  add rax, rdi\n");
+        break;
+    case '-':
+        printf("  sub rax, rdi\n");
+        break;
+    default:
+        error("知らないノード種別: %d\n", node->ty);
+        break;
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
@@ -88,43 +177,22 @@ int main(int argc, char **argv) {
     // トークナイズする
     user_input = argv[1];
     tokenize();
+    Node *node = expr();
+
+    if (!consume(TK_EOF))
+        error_at(tokens[pos].input, "EOFでありません");
 
     // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないので、それをチェックして
-    // 最初のmov命令を出力
-    if (tokens[0].ty != TK_NUM)
-        error_at(tokens[0].input, "数ではありません");
-    printf("  mov rax, %d\n", tokens[0].val);
+    // コード生成
+    gen(node);
 
-    // '+ <数>' あるいは'- <数>'というトークンの並びを消費しつつ
-    // アセンブリを出力
-    int i = 1;
-    while (tokens[i].ty != TK_EOF) {
-        if (tokens[i].ty == '+') {
-            i++;
-            if (tokens[i].ty != TK_NUM)
-                error_at(tokens[i].input, "数ではありません");
-            printf("  add rax, %d\n", tokens[i].val);
-            i++;
-            continue;
-        }
-
-        if (tokens[i].ty == '-') {
-            i++;
-            if (tokens[i].ty != TK_NUM)
-                error_at(tokens[i].input, "数ではありません");
-            printf("  sub rax, %d\n", tokens[i].val);
-            i++;
-            continue;
-        }
-
-        error_at(tokens[i].input, "予期しないトークンです");
-    }
-
+    // スタックトップに残っている式全体の値を
+    // RAXにロードして関数からの返値にする
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
